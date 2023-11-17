c689a253646b985a07ba7916c29230ff182cdb52
soultan amirul mukminin <109590635+soul222@users.noreply.github.com>
2023-11-17 14:12:49 +0700

    Update NewsController.php

--- rest-api/app/Http/Controllers/NewsController.php
+++ rest-api/app/Http/Controllers/NewsController.php
@@ -3,18 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\news;
+use App\Models\News;
 
 class NewsController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index(Request $request)
     {
         try {
+            // Mendapatkan nilai pencarian dari parameter 'search' pada URL
             $query = $request->input('search');
 
+            // Melakukan pencarian berdasarkan judul atau kategori jika ada nilai pencarian
             $news = News::when($query, function ($queryBuilder) use ($query) {
                 $queryBuilder->where('title', 'like', "%$query%")
                              ->orWhereHas('category', function ($categoryQuery) use ($query) {
@@ -22,13 +27,16 @@ class NewsController extends Controller
                              });
             })->with('author', 'category')->get();
 
+            // Jika hasil pencarian kosong, lempar exception dengan pesan yang sesuai
             if ($news->isEmpty()) {
-                $message = $query ? "Resource not found for the search: $query" : 'Data Empty';
+                $message = $query ? "Data not found for the search: $query" : 'Data Empty';
                 throw new \Exception($message);
             }
 
+            // Menyiapkan pesan berdasarkan apakah ada pencarian atau tidak
             $searchMessage = $query ? "Resource search for '$query' is successful" : 'Get all resource';
 
+            // Menyiapkan respons JSON dengan pesan dan data berita
             $response = [
                 'message' => $searchMessage,
                 'data' => $news,
@@ -36,6 +44,7 @@ class NewsController extends Controller
 
             return response()->json($response, 200);
         } catch (\Exception $e) {
+            // Jika terjadi exception, tangkap pesannya dan kirimkan sebagai respons JSON
             $response = [
                 'message' => $e->getMessage(),
             ];
@@ -46,10 +55,14 @@ class NewsController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
         try {
+            // Validasi data yang diterima dari permintaan
             $data = $request->validate([
                 'title' => 'required',
                 'description' => 'required',
@@ -61,11 +74,13 @@ class NewsController extends Controller
                 'category_id' => 'required|exists:categories,id',
             ]);
 
+            // Membuat berita baru dan menyimpannya ke dalam database
             $news = News::create($data);
 
-            // Load informasi penulis dan kategori
+            // Memuat informasi penulis dan kategori
             $news->load('author', 'category');
 
+            // Menyiapkan respons JSON dengan pesan dan data berita yang baru dibuat
             $response = [
                 'message' => 'Resource is added successfully',
                 'data' => $news,
@@ -73,21 +88,28 @@ class NewsController extends Controller
 
             return response()->json($response, 201);
         } catch (\Exception $e) {
+            // Jika terjadi exception, tangkap pesannya dan kirimkan sebagai respons JSON
             $response = [
                 'message' => $e->getMessage(),
             ];
+
             return response()->json($response, 400);
         }
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show($id)
     {
         try {
-            $news = news::find($id);
+            // Menemukan berita berdasarkan ID
+            $news = News::find($id);
 
+            // Jika berita ditemukan, siapkan respons JSON dengan pesan dan data berita
             if ($news) {
                 $response = [
                     'message' => 'Get detail resource',
@@ -96,65 +118,34 @@ class NewsController extends Controller
 
                 return response()->json($response, 200);
             } else {
+                // Jika berita tidak ditemukan, lempar exception dengan pesan 'Resource not found'
                 throw new \Exception('Resource not found');
             }
         } catch (\Exception $e) {
+            // Jika terjadi exception, tangkap pesannya dan kirimkan sebagai respons JSON
             $response = [
                 'message' => $e->getMessage(),
             ];
+
             return response()->json($response, 404);
         }
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $id
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, string $id)
     {
         try {
-            $news = news::find($id);
+            // Menemukan berita berdasarkan ID
+            $news = News::find($id);
 
+            // Jika berita ditemukan, siapkan respons JSON dengan pesan dan data berita yang diperbarui
             if ($news) {
                 $response = [
                     'message' => 'Resource is added successfully',
-                    'data' => $news->update($request->all()),
-                ];
-
-                return response()->json($response, 200);
-            } else {
-                throw new \Exception('Resource not found');
-            }
-        } catch (\Exception $e) {
-            $response = [
-                'message' => $e->getMessage(),
-            ];
-            return response()->json($response, 400);
-        }
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        try {
-            $news = news::find($id);
-
-            if ($news) {
-                $response = [
-                    'message' => 'Resource is delete successfully',
-                    'data' => $news->delete(),
-                ];
-
-                return response()->json($response, 200);
-            } else {
-                throw new \Exception('Data not found');
-            }
-        } catch (\Exception $e) {
-            $response = [
-                'message' => $e->getMessage(),
-            ];
-            return response()->json($response, 404);
-        }
-    }
-}
+                    'data' => $
